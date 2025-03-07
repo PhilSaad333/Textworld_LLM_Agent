76e07e400460988be191a270a977e4952b3705cf
Phil Saad <phil.saad333@gmail.com>
2025-03-06 16:29:36 -0800

    small changes

--- environment/__init__.py
+++ environment/__init__.py
@@ -1,4 +1,4 @@
 from .gym_wrapper import GymWrapper
 from .environment_manager_old import DifficultyEnvironmentManager
-from .task_env import TaskEnvManager
+from .task_env import TaskEnvManager, TaskConfig
 
